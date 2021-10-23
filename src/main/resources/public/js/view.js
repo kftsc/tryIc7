f02c45e8351589794ee1bcc968a88da74fd00ba6
kftsc <kftsc3@gmail.com>
2021-10-23 13:21:39 -0500

    1111

---
+++ src/main/resources/public/js/view.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/chatapp");
+
+/**
+ * Entry point into chat room
+ */
+window.onload = function() {
+
+    webSocket.onclose = () => alert("WebSocket connection closed");
+    webSocket.onmessage = (msg) => updateChatRoom(msg);
+
+    $("#btn-msg").click(() => sendMessage($("#message").val()));
+};
+
+/**
+ * Send a message to the server.
+ * @param msg  The message to send to the server.
+ */
+function sendMessage(msg) {
+    if (msg !== "") {
+        webSocket.send(msg);
+        $("#message").val("");
+    }
+}
+
+/**
+ * Update the chat room with a message.
+ * @param message  The message to update the chat room with.
+ */
+function updateChatRoom(message) {
+    // TODO: convert the data to JSON and use .append(text) on a html element to append the message to the chat area
+    //console.log(message);
+    let data = JSON.parse(message.data);
+    $("#chatArea").append(data.userMessage);
+
+}
